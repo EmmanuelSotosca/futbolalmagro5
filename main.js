31ace99337c9b5b5d7dc7c9e6d4a47b7e2df1c5b
EmmanuelSotosca <105554563+EmmanuelSotosca@users.noreply.github.com>
2023-09-24 16:08:57 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+let nombre = "Emmanuel";
+let edad = "30";
+
+const IVA = 0.21;
+console.log("hola a todos",nombre);
+console.log(nombre,edad);
+
+let numeroUno = 5; 
+let numeroDos = 5;
+
+ 
+let resultadoSuma=numeroUno + numeroDos;
+let resultadoResta=numeroUno - numeroDos;
+let resultadoMulti=numeroUno * numeroDos;
+let resultadoDivi=numeroUno / numeroDos;
+
+console.log("resultado de la suma",resultadoSuma);
+console.log("resultado de la suma",resultadoResta);
+console.log("resultado de la suma",resultadoMulti);
+console.log("resultado de la suma",resultadoDivi);
+
+let numero= Number (prompt("ingrese el numero"));
+alert(numero+13);
